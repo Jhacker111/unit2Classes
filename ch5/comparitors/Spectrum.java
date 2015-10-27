5ed10007c3ec0f8d89b701224221510c6c3705a3
Jhacker111 <Jhhacker@NN121-HA-09.HS.sd203.org>
2015-10-27 10:30:01 -0500

    adsf

---
+++ ch5/comparitors/Spectrum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Spectrum
+{
+    public static void main(String[] args)
+    {
+        System.out.println("What wavelength would you like to enter? (Use 1e-1 for format)");
+        Scanner wavelengthIn = new Scanner(System.in);
+        float wavelength = wavelengthIn.nextFloat();
+        if(wave>1e-1 && 
+        {
+            
+    }
+}
